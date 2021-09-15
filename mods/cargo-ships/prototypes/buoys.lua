82a1554e25639c26eb9d89bfbbc808dc934adef5
amirhh00 <amirhh00@hotmail.com>
2021-09-16 02:04:33 +0430

    boats

---
+++ mods/cargo-ships/prototypes/buoys.lua
@@ -0,0 +1,222 @@
+local floating_pole=table.deepcopy(data.raw["electric-pole"]["big-electric-pole"])
+floating_pole.name = "floating-electric-pole"
+floating_pole.minable ={mining_time = 0.5, result = "floating-electric-pole"}
+floating_pole.collision_mask = {'ground-tile', 'object-layer'}
+floating_pole.maximum_wire_distance = 48
+floating_pole.supply_area_distance = 0
+floating_pole.fast_replaceable_group = nil
+floating_pole.next_upgrade = nil
+floating_pole.pictures =
+    {
+      filename =  "__cargo-ships__/graphics/entity/floating_electric_pole/floating-electric-pole.png",
+      priority = "high",
+      width = 168,
+      height = 165,
+      direction_count = 4,
+      shift = {1.61, -1.87}
+    }
+floating_pole.connection_points =
+  {
+    { -- Vertical
+      shadow =
+      { copper = {2.78, -0.5},
+        green = {1.875, -0.5},
+        red = {3.69, -0.5} },
+      wire =
+      { copper = {0, -4.05},
+        green = {-0.59375, -4.05},
+        red = {0.625, -4.05} }
+    },
+    { -- Turned right
+      shadow =
+      { copper = {3.1, -0.648},
+        green = {2.3, -1.144},
+        red = {3.8, -0.136} },
+      wire =
+      { copper = {-0.0525, -3.906},
+        green = {-0.48, -4.179},
+        red = {0.36375, -3.601} }
+    },
+    { -- Horizontal
+      shadow =
+      { copper = {2.9, -0.564},
+        green = {3.0, -1.316},
+        red = {3.0, 0.152} },
+      wire =
+      { copper = {-0.09375, -3.901},
+        green = {-0.09375, -4.331},
+        red = {-0.09375, -3.420} }
+    },
+    { -- Turned left
+      shadow =
+      { copper = {3.3, -0.542},
+        green = {3.1, -1.058},
+        red = {2.35, -0.035} },
+      wire =
+      { copper = {-0.0625, -3.980},
+        green = {0.375, -4.273},
+        red = {-0.46875, -3.656} }
+    }
+  }
+
+local buoy=table.deepcopy(data.raw["rail-signal"]["rail-signal"])
+buoy.name = "buoy"
+buoy.collision_mask = {'ground-tile', 'rail-layer'}
+buoy.selection_box =  {{-1.6, -0.8}, {0.01, 0.8}}
+buoy.fast_replaceable_group = "buoy"
+buoy.minable = {mining_time = 0.5, result = "buoy"}
+buoy.animation =
+{
+  filename = "__cargo-ships__/graphics/entity/buoy/buoys.png",
+  priority = "high",
+  width = 144,
+  height = 144,
+  frame_count = 3,
+  direction_count = 8,
+  scale = 0.8
+}
+buoy.rail_piece = nil
+
+
+local chain_buoy=table.deepcopy(data.raw["rail-chain-signal"]["rail-chain-signal"])
+chain_buoy.name = "chain_buoy"
+chain_buoy.collision_mask = {'ground-tile', 'rail-layer'}
+chain_buoy.selection_box =  {{-1.6, -0.8}, {0.01, 0.8}}
+chain_buoy.fast_replaceable_group = "buoy"
+chain_buoy.minable = {mining_time = 0.5, result = "chain_buoy"}
+chain_buoy.animation =
+{
+  filename = "__cargo-ships__/graphics/entity/chain_buoy/chain_buoys.png",
+  priority = "high",
+  width = 256,
+  height = 256,
+  frame_count = 5,
+  direction_count = 8,
+  scale = 0.8,
+  shift = {0.5,0.3}
+
+}
+chain_buoy.rail_piece = nil
+
+
+
+
+
+local port=table.deepcopy(data.raw["train-stop"]["train-stop"])
+port.name = "port"
+port.icon = "__cargo-ships__/graphics/blank.png"
+port.minable = {mining_time = 1, result = "port"}
+port.rail_overlay_animations = nil
+port.collision_mask = {"object-layer"}
+port.collision_box =  {{-0.01, -0.9}, {1.9, 0.9}}
+port.selection_box =  {{-0.01, -0.9}, {1.9, 0.9}}
+port.animations = make_4way_animation_from_spritesheet({ layers =
+  {
+    {
+      filename = "__cargo-ships__/graphics/entity/port/uniport.png",
+      width = 140,
+      height = 291,
+      direction_count = 4,
+      shift = util.by_pixel(-0.5, -26.75),
+      scale = 0.8
+    }
+  }
+})
+port.top_animations = nil
+port.light1 =
+{
+  light = {intensity = 0.5, size = 5, color = {r = 1.0, g = 1.0, b = 1.0}},
+  picture =
+  {
+    north =
+    {
+      filename = "__cargo-ships__/graphics/entity/port/port-light-1.png",
+      width =14,
+      height = 7,
+      scale = 0.7,
+      shift = util.by_pixel(36.5, -67.5),
+    },
+    west =
+    {
+      filename = "__cargo-ships__/graphics/entity/port/port-light-1.png",
+      width =14,
+      height = 7,
+      scale = 0.7,
+      shift = util.by_pixel(-2, -104.3),
+    },
+    south =
+    {
+      filename = "__cargo-ships__/graphics/entity/port/port-light-1.png",
+      width =14,
+      height = 7,
+      scale = 0.7,
+      shift = util.by_pixel(-39.5, -66),
+    },
+    east =
+    {
+      filename = "__cargo-ships__/graphics/entity/port/port-light-1.png",
+      width =14,
+      height = 7,
+      scale = 0.7,
+      shift = util.by_pixel(-2.7, -26.5),
+    },
+  },
+  red_picture =
+   {
+    north =
+    {
+      filename = "__cargo-ships__/graphics/entity/port/port-red-light-1.png",
+      width = 14,
+      height = 7,
+      frame_count = 1,
+      shift = util.by_pixel(36.5, -67.5),
+    },
+    west =
+    {
+      filename = "__cargo-ships__/graphics/entity/port/port-red-light-1.png",
+      width = 14,
+      height = 7,
+      frame_count = 1,
+      shift = util.by_pixel(-2, -104.3),
+    },
+    south =
+    {
+      filename = "__cargo-ships__/graphics/entity/port/port-red-light-1.png",
+      width = 14,
+      height = 7,
+      frame_count = 1,
+      shift = util.by_pixel(-39.5, -66),
+    },
+    east =
+    {
+      filename = "__cargo-ships__/graphics/entity/port/port-red-light-1.png",
+      width = 14,
+      height = 7,
+      frame_count = 1,
+      shift = util.by_pixel(-2.7, -26.5),
+    },
+  }
+}
+port.light2 = nil
+port.working_sound = nil
+
+-- build a new 4 way definition for port
+-- show_shadow=false prevents floating circuit box shadows, but wire shadows end nowhere
+-- once port shadows are done set show_shadow=true and tweak shadow_offset, should be around (-30, 10) from  main_offset
+circuit_connector_definitions["cargo-ships-port"] = circuit_connector_definitions.create
+(
+  universal_connector_template,
+  {
+    { variation = 18, main_offset = util.by_pixel(37, -61), shadow_offset = util.by_pixel(37, -61), show_shadow = false },
+    { variation = 18, main_offset = util.by_pixel(-1.5, -20), shadow_offset = util.by_pixel(-1.5, -20), show_shadow = false },
+    { variation = 18, main_offset = util.by_pixel(-39, -59), shadow_offset = util.by_pixel(-39, -59), show_shadow = false },
+    { variation = 18, main_offset = util.by_pixel(-1.5, -98), shadow_offset = util.by_pixel(-1.5, -98), show_shadow = false }
+  }
+)
+-- let factorio generate sprite connector offset per wire from definition
+port.circuit_wire_connection_points = circuit_connector_definitions["cargo-ships-port"].points
+port.circuit_connector_sprites = circuit_connector_definitions["cargo-ships-port"].sprites
+
+
+data:extend({buoy, chain_buoy, port, floating_pole})
+
---
+++ mods/cargo-ships/prototypes/oil_rig.lua
@@ -0,0 +1,292 @@
+----------------------------------------------------------------
+--------------------------- PUMP -------------------------------
+----------------------------------------------------------------
+
+local ship_pump=table.deepcopy(data.raw["pump"]["pump"])
+ship_pump.name = "ship_pump"
+ship_pump.minable = {mining_time = 1, result = "ship_pump"}
+ship_pump.collision_mask = {"object-layer"}
+ship_pump.pumping_speed = 1000
+ship_pump.energy_usage = "50kW"
+ship_pump.next_upgrade = nil
+
+
+
+
+local pump_marker=table.deepcopy(data.raw["simple-entity-with-owner"]["simple-entity-with-owner"])
+pump_marker.name = "pump_marker"
+pump_marker.flags = {"not-repairable", "not-blueprintable", "not-deconstructable", "placeable-off-grid", "not-on-map"}
+pump_marker.selectable_in_game = false
+pump_marker.allow_copy_paste = false
+pump_marker.render_layer = "selection-box"
+pump_marker.minable = nil
+pump_marker.collision_mask = {}
+pump_marker.picture =
+{
+  filename = "__cargo-ships__/graphics/green_selection_box.png",
+  width = 128,
+  height = 128,
+  scale = 0.5,
+  frame_count = 1
+}
+
+
+----------------------------------------------------------------
+-------------------------DEEP SEA OIL --------------------------
+----------------------------------------------------------------
+
+local deep_oil=table.deepcopy(data.raw["resource"]["crude-oil"])
+if mods["angelspetrochem"] then
+  deep_oil.minable = 
+  {
+    hardness = 1,
+    mining_time = 1, 
+    results = 
+    {
+      {
+        type = "fluid", 
+        name = "liquid-multi-phase-oil", 
+        amount_min = 10, 
+        amount_max = 10, 
+        probability = 1
+      }
+    }
+  }
+
+end
+deep_oil.name = "deep_oil"
+deep_oil.infinite_depletion_amount = 40
+deep_oil.autoplace = nil
+deep_oil.collision_mask = {'resource-layer'}
+
+
+
+
+
+
+----------------------------------------------------------------
+------------------------ OIL PLATFORM --------------------------
+----------------------------------------------------------------
+local oil_rig_capacity = settings.startup["oil_rig_capacity"].value;
+
+
+local oil_rig=table.deepcopy(data.raw["mining-drill"]["pumpjack"])
+oil_rig.name = "oil_rig"
+oil_rig.collision_mask = {'object-layer', "train-layer"}
+oil_rig.minable = {mining_time = 3, result = "oil_rig"}
+oil_rig.dying_explosion = "big-explosion"
+oil_rig.max_health = 1000
+oil_rig.energy_usage = "750kW"
+oil_rig.mining_speed = 2
+oil_rig.resource_searching_radius = 1.49
+oil_rig.collision_box = {{ -3.2, -3.2}, {3.2, 3.2}}
+oil_rig.selection_box = {{ -3.3, -3.3}, {3.3, 3.3}}
+oil_rig.drawing_box = {{-3.3, -3.3}, {3.3, 8}}
+oil_rig.module_specification.module_slots = 3;
+oil_rig.energy_source = 
+{
+  type = "electric",
+  -- will produce this much * energy pollution units per tick
+  emissions = 0.2/60,
+  usage_priority = "secondary-input"
+}
+oil_rig.output_fluid_box =
+{
+  base_area = 10,
+  base_level = 10,
+  height = 2*oil_rig_capacity,
+  pipe_covers = pipecoverspictures(),
+  pipe_connections =
+  {
+    { position = {0, -4}}
+  },
+}
+oil_rig.base_picture =
+{
+  sheets =
+  {
+    {
+      filename = "__cargo-ships__/graphics/entity/oil_rig/oil_rig.png",
+      priority = "very-low",
+      width = 300,
+      height = 471,
+      shift = util.by_pixel(-2.5, -29),
+      scale = 0.9,
+    }
+  }
+}
+oil_rig.animations = 
+{
+  north =
+  {
+    layers = 
+    {
+      {
+        priority = "high",
+        filename = "__cargo-ships__/graphics/blank.png",
+        line_length = 8,
+        width = 2,
+        height = 2,
+        frame_count = 40,
+        shift = util.by_pixel(-4, -24),
+        animation_speed = 0.5,
+      }
+    }
+  }
+}
+oil_rig.smoke =
+{
+  {
+    name = "light-smoke",
+    north_position = {-3.2, -2.5},
+    frequency = 0.5,
+    starting_vertical_speed = 0.08,
+    slow_down_factor = 1,
+    starting_frame_deviation = 60
+  },
+  {
+    name = "smoke",
+    north_position = {1.4, -2.5},
+    frequency = 1,
+    starting_vertical_speed = 0.12,
+    slow_down_factor = 1,
+    starting_frame_deviation = 60
+  }
+}
+
+
+
+----------------------------------------------------------------
+----------- OIL PLATFORM SLAVE ENTITES--------------------------
+----------------------------------------------------------------
+
+local or_power=table.deepcopy(data.raw["generator"]["steam-engine"])
+or_power.flags = {"not-blueprintable", "not-deconstructable"}
+or_power.selectable_in_game = false
+or_power.allow_copy_paste = false
+or_power.name = "or_power"
+or_power.collision_box= nil
+or_power.selection_box= nil
+or_power.collision_mask= {}
+or_power.fast_replaceable_group = nil
+or_power.next_upgrade = nil
+or_power.fluid_usage_per_tick = 1
+or_power.fluid_box =
+    {
+      base_area = 1,
+      height = 1,
+      base_level = -1,
+      pipe_covers = nil,
+      pipe_connections = {},
+      production_type = "input-output",
+      filter = "steam",
+      minimum_temperature = 100.0
+    }
+or_power.horizontal_animation = 
+{
+  layers =
+        {
+          {
+            filename = "__cargo-ships__/graphics/blank.png",
+            width = 2,
+            height = 2,
+            frame_count = 32,
+            line_length = 8,
+          }
+        }
+}
+or_power.vertical_animation = 
+{
+  layers =
+  {
+    {
+      filename = "__cargo-ships__/graphics/blank.png",
+      width = 2,
+      height = 2,
+      frame_count = 32,
+      line_length = 8,
+    }
+  }
+}
+or_power.smoke =
+{
+  {
+    name = "light-smoke",
+    north_position = {-3.2, -4.5},
+    frequency = 0.5,
+    starting_vertical_speed = 0.08,
+    slow_down_factor = 1,
+    starting_frame_deviation = 60
+  },
+  {
+    name = "smoke",
+    north_position = {1.4, -4.5},
+    frequency = 1,
+    starting_vertical_speed = 0.12,
+    slow_down_factor = 1,
+    starting_frame_deviation = 60
+  }
+}
+
+
+
+local or_pole=table.deepcopy(data.raw["electric-pole"]["medium-electric-pole"])
+or_pole.name = "or_pole"
+or_pole.flags = {"not-blueprintable", "not-deconstructable"}
+or_pole.selectable_in_game = false
+or_pole.allow_copy_paste = false
+or_pole.collision_box= nil
+or_pole.selection_box= nil
+or_pole.collision_mask= {}
+or_pole.fast_replaceable_group = nil
+or_pole.next_upgrade=nil
+or_pole.maximum_wire_distance = 0
+or_pole.pictures =
+{
+  filename = "__cargo-ships__/graphics/blank.png",
+  width = 2,
+  height = 2,
+  direction_count = 4,
+  line_length = 4,
+}
+or_pole.supply_area_distance = 4.5
+
+
+
+local or_lamp=table.deepcopy(data.raw["lamp"]["small-lamp"])
+or_lamp.name = "or_lamp"
+or_lamp.flags = {"not-blueprintable", "not-deconstructable"}
+or_lamp.selectable_in_game = false
+or_lamp.allow_copy_paste = false
+or_lamp.collision_box= nil
+or_lamp.selection_box= nil
+or_lamp.collision_mask= {}
+or_lamp.picture_off = 
+{  
+  filename = "__cargo-ships__/graphics/blank.png",
+  width = 2,
+  height = 2,
+}
+or_lamp.pciture_on = {}
+
+local or_radar=table.deepcopy(data.raw["radar"]["radar"])
+or_radar.name= "or_radar"
+or_radar.flags = {"not-blueprintable", "not-deconstructable"}
+or_radar.selectable_in_game = false
+or_radar.allow_copy_paste = false
+or_radar.collision_mask={}
+or_radar.collision_box= nil
+or_radar.selection_box= nil
+or_radar.pictures=
+{
+  filename = "__cargo-ships__/graphics/blank.png",
+  width = 2,
+  height = 2,
+  direction_count = 4,
+  line_length = 4,
+}
+or_radar.max_distance_of_sector_revealed = 0
+or_radar.energy_usage = "50kW",
+
+data:extend({ship_pump, pump_marker, oil_rig, or_power, or_pole, or_radar, or_lamp, deep_oil})
+
